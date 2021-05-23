c550dc0865569fc53147c67fedef4ac6eb862bb0
Ahmed Saleem <ahmed4u@gmail.com>
2021-05-23 07:00:34 +0000

    Terraform main file added

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "ap-southeast-1"
 }
 module "kube_cluster1" {
-  source  = "./kube_cluster"
+  source  = "./kube_cluster1"
 }
 output "nodes" {
   value = module.cluster.cluster_nodes
